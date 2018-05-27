a0be1e61f28e6b34a623881f287afe30bb69b46d
Ed Garrity <EdGarrity11375@gmail.com>
2018-05-26 21:30:34 -0400

    Chapter 6

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -1,13 +1,23 @@
 (ns chatter.handler
+  (:gen-class)
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.params :refer [wrap-params]]
+            [ring.adapter.jetty :as jetty]
             [hiccup.page :as page]
-            [hiccup.form :as form]))
-
+            [hiccup.form :as form]
+            [ring.util.anti-forgery :as anti-forgery]
+            [environ.core :refer [env]]))
+			
 (def chat-messages (atom '()))
 
+(defn init []
+  (println "chatter is starting"))
+
+(defn destroy []
+  (println "chatter is shutting down"))
+  
 (defn generate-message-view
   "This generates the HTML for displaying messages"
   [messages]
@@ -15,7 +25,8 @@
    [:head
     [:title "chatter"]
     (page/include-css "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css")
-    (page/include-js  "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js")]
+    (page/include-js  "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js")
+	(page/include-css "/chatter.css")]
    [:body
     [:h1 "Our Chat App"]
     [:p
@@ -25,7 +36,7 @@
       "Message: " (form/text-field "msg")
       (form/submit-button "Submit"))]
     [:p
-     [:table#messages.table.table-striped
+     [:table#messages.table.table-hover
       (map (fn [m] [:tr [:td (:name m)] [:td (:message m)]]) messages)]]]))
 
 (defn update-messages!
@@ -41,6 +52,12 @@
           new-messages (update-messages! chat-messages name-param msg-param)]
       (generate-message-view new-messages)
       ))
+  (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app (wrap-params app-routes))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty #'app {:port port :join? false})))
+	
